2c9ef8afcdf17d197283a75b168029821bb300db
Tomosuke Okada <orenoti@gmail.com>
2021-09-19 11:01:12 +0900

    add files.

---
+++ Pokedex-SwiftUI/PokedexApp.swift
@@ -0,0 +1,18 @@
+//
+//  Pokedex_SwiftUIApp.swift
+//  Shared
+//
+//  Created by Tomosuke Okada on 2021/09/19.
+//
+
+import SwiftUI
+
+@main
+struct PokedexApp: App {
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
